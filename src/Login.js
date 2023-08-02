4a4692058a9eea876fa6e9c052522585755bf6c7
Aslınur Tolga <aslinurtolga@metaworldbilisim.com>
2023-08-02 15:13:25 +0300

    feat: form test done, logged in screen came up

--- src/Login.js
+++ src/Login.js
@@ -21,55 +21,67 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(user,pwd);
-    setUser('');
-    setPwd('');
+    console.log(user, pwd);
+    setUser("");
+    setPwd("");
     setSuccess(true);
   };
 
   return (
-    <section>
-      <p
-        ref={errRef}
-        className={errMsg ? "errmsg" : "offscreen"}
-        aria-live="assertive"
-      >
-        {errMsg}
-      </p>
-      <h1>Sign In</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          ref={userRef}
-          autoComplete="off"
-          onChange={(e) => {
-            setUser(e.target.value);
-          }}
-          value={user}
-          required
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="text"
-          id="password"
-          onChange={(e) => {
-            setPwd(e.target.value);
-          }}
-          value={pwd}
-          required
-        />
-        <button>Sign In</button>
-      </form>
-      <p>
-        Need an Account? <br />
-        <span className="line">
-          {/* put router link here */}
-          <a href="#">Sign Up</a>
-        </span>
-      </p>
-    </section>
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => {
+                setUser(e.target.value);
+              }}
+              value={user}
+              required
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="text"
+              id="password"
+              onChange={(e) => {
+                setPwd(e.target.value);
+              }}
+              value={pwd}
+              required
+            />
+            <button>Sign In</button>
+          </form>
+          <p>
+            Need an Account? <br />
+            <span className="line">
+              {/* put router link here */}
+              <a href="#">Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )}
+    </>
   );
 };
 
